ef9054957d11bf28e458b5763d3a49f92d5eb66a
Ruslan <rusya86hmrus@gmail.com>
2024-02-22 12:08:19 +0500

    finish3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import Calculate.Calculate;
+import Calculate.CalculateExprassion;
+import Reader.Reader;
+import java.util.List;
+
+import Reader.ReadExpression;
+import logger.Controller.LogerControllerCalculatte;
+import logger.Controller.LogerController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Reader r = new ReadExpression();
+        LogerController logger = new LogerControllerCalculatte();
+        Calculate calculate = new CalculateExprassion(logger);
+        List<String> exp = r.read();
+        float res = calculate.calculate(exp);
+        System.out.println("Ответ: " + res);
+        logger.printLog();
     }
 }
